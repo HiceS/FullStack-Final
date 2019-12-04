42b0120150430c5187e6558157e22ba6479316cb
Shawn Hice <shawnhice@gmail.com>
2019-12-04 11:22:52 -0800

    Added assets

--- src/components/HNav.jsx
+++ src/components/HNav.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import {Navbar, Nav} from 'react-bootstrap';
 
+import Logo from './../assets/robotic.png';
+
 function HNav () {
     return(
         <Navbar className="header-container" fixed="top" expand="lg" variant="dark" bg="dark">
-            <Navbar.Brand>Shawn Hice</Navbar.Brand>
+            <Navbar.Brand>
+                <img
+                    alt=""
+                    src={Logo}
+                    width="30"
+                    height="30"
+                    className="d-inline-block align-top"
+                />{' '}
+                  Harnec
+            </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href="https://github.com/HiceS">Github</Nav.Link>
-                    <Nav.Link href="mailto:shice@pdx.edu">Email</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
